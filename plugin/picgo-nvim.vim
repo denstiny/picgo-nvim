5b977abbfff67d36ca4920b436399b84d0e5d3eb
denstiny <2254228017@qq.com>
2022-02-28 11:42:00 +0800

    fead:first push

--- plugin/picgo-nvim.vim
+++ plugin/picgo-nvim.vim
@@ -1,6 +1,111 @@
-" Author        : aero
-" Created       : 27/02/2022
-" License       : MIT
-" Description   : 自动上传文件到图床
+" @author      : aero (2254228017@qq.com)
+" @file        : picgo-nvim
+" @created     : 星期日 2月 27, 2022 16:09:20 CST 
+" @github      : https://github.com/denstiny
+" @blog        : https://denstiny.github.io
 
 
+"==============================================
+function! s:optdef(argument, default)
+  if !has_key(g:, a:argument) | let g:{a:argument} = a:default | endif
+endfunction
+
+"==============================================
+" 启动插件
+call s:optdef("picgo_start", 1)
+" 自动转换地址
+call s:optdef("picgo_auto_path", 0)
+" 运行启动的文件类型
+call s:optdef("picgo_run_list", [''])
+call s:optdef("picgo_key","")
+
+"==============================================
+
+let s:py_file = expand('<sfile>:p:h') . '/../script/imagePost.py' 
+if has('python')
+  let s:python_executable = 'python'
+elseif has('python3')
+  let s:python_executable = 'python3'
+else
+  echo "No \"python\" provider found"
+endif
+let s:xclip_start = v:false
+
+"==============================================
+function! s:picgo_output(jobpid,data,event) abort
+  if !empty(a:data)
+    echo a:data
+  endif
+endfunction
+let s:line = v:false
+let s:g__ = ' '
+
+"==============================================
+function! s:on_stdout(id,data,event) abort
+  if len(s:g__) > 1
+    return
+  else
+    let s:g__ = join(a:data,' ')
+  endif
+endfunction
+"==============================================
+function s:on_exit(id,data,event) abort
+  if len(s:g__) > 1
+    echo s:g__
+    call append(s:line, s:g__)
+  endif
+  let s:g__ = ' '
+endfunction
+"==============================================
+function! s:on_stderr(id,data,event) abort
+  if len(s:g__) > 1
+    return
+  else
+    let s:g__ = join(a:data,' ')
+  endif
+endfunction
+
+"==============================================
+"=== 有一说一代码改抄还得抄,节省时间干别的 ====
+function! s:get_visual_selection()
+  let s:line = line('.')
+  let [line_start, column_start] = getpos("'<")[1:2]
+  let [line_end, column_end] = getpos("'>")[1:2]
+  let lines = getline(line_start, line_end)
+  if len(lines) == 0
+    return ''
+  endif
+  let lines[-1] = lines[-1][: column_end - (&selection == 'inclusive' ? 1 : 2)]
+  let lines[0] = lines[0][column_start - 1:]
+  call UpdateHiddenSymbols()
+  return join(lines, '')
+endfunction
+"==============================================
+function! s:UpdateHiddenSymbols(argcomment) abort
+  let s:line = line('.')
+  let s:loop = s:py_file
+  let s:callbacks = {
+        \ 'on_stdout':function('s:on_stdout'),
+        \ 'on_stderr':function('s:on_stderr'),
+        \ 'on_exit':function('s:on_exit')
+        \}
+  if a:argcomment == v:false
+    call jobstart([s:python_executable,s:loop],s:callbacks)
+  else
+    call jobstart([s:python_executable,s:loop,a:argcomment],s:callbacks)
+  endif
+endfunction
+"==============================================
+function g:UpdateImageR() abort
+  let s:xclip_start = v:false
+  let s:virtual_line = s:get_visual_selection()
+  call s:UpdateHiddenSymbols(s:virtual_line)
+endfunction
+"==============================================
+function g:UpdateImageX() abort
+  let s:xclip_start = v:true
+  call s:UpdateHiddenSymbols(v:false)
+endfunction
+"==============================================
+nmap <silent><A-p> :call UpdateImageX()<cr> " 上传剪切板图片
+vmap <silent><A-o> :call UpdateImageR()<cr> " 上传指定位置图片
---
+++ script/imagePost.py
@@ -0,0 +1,145 @@
+"""
+
+ @author      : aero (2254228017@qq.com)
+ @file        : imagePost
+ @created     : 星期六 2月 26, 2022 20:08:57 CST
+ @github      : https://github.com/denstiny
+ @blog        : https://denstiny.github.io
+
+"""
+
+import os
+import json
+import sys
+import requests
+import subprocess
+import datetime
+
+
+
+root = {'遇见图床': {'url':'https://www.hualigs.cn/api/upload',
+        'token':'eaa4eb11884f7a60e03f5c2eab3161c8'},
+        '牛图网'  : {'url':'https://www.niupic.com/api/upload',
+        'token':'xxx'}}
+
+
+
+class PostImage:
+    def __init__(self):
+        self.xclip_path = ' '
+        pass
+
+    def currentTime(self,filetype):
+        """
+
+        Args:
+            filetype (): 文件后缀
+
+        Returns: 返回当前时间的文件名称
+            
+        """
+        time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        return time+filetype
+
+    def PostImage(self, head,filedir):
+        """
+        上传图片到图床
+        Args:
+            head (): 请求头字典
+            files (): 文件包装数组
+
+        Returns:
+            返回图片链接,否则返回False
+        """
+        data = { "apiType":'majorhua', "token":head['token'] }
+        file = self.FileOpen(filedir)
+        if file ==  False:
+            return False
+        res = requests.post(url= head['url'],data=data,files=file['files'])
+        jsonString = res.content.decode()
+        userData = json.loads(jsonString)
+        if userData['code'] != 200 and userData['msg'] != 'success':
+            print("error:Upload failed",file=sys.stderr)
+            return False
+        file['image'].close()
+        return userData['data']['url']['distribute']
+
+    def FileOpen(self,filePath):
+        """
+
+        Args:
+            filePath (): 文件路径
+
+        Returns:
+            返回包装好的文件数组
+        """
+        if os.path.exists(filePath) ==  False:
+            print("error:No such file or directory",sys.stderr)
+            return False
+        image = open(filePath,'rb')
+        imageName = os.path.basename(filePath)
+        files = {"image":(imageName,image,'image/jpeg')}
+        return {"files":files,"image":image}
+
+    def get_xclip(self,cmd):
+        """
+        执行查找和保存图片的命令，并返回状态
+        Args:
+            cmd (): 执行的shell命令
+
+        Returns:
+            成功 返回True 失败 返回False
+        """
+
+        sub = subprocess.run(cmd,shell=True,stdout=subprocess.PIPE)
+        return sub.returncode
+
+    def getClipFileImage(self):
+        """ 获取剪切板图片 """
+        has_png = "xclip -selection clipboard -t TARGETS -o | grep image/png"
+        has_jpg = "xclip -selection clipboard -t TARGETS -o | grep image/jpg"
+        save_png = "xclip -selection clipboard -t image/png -o > "
+        save_jpg = "xclip -selection clipboard -t image/jpg -o > "
+        if self.get_xclip(has_png) ==  0:
+            file_path = self.currentTime('.png')
+            self.get_xclip(save_png + file_path)
+            image_url = self.PostImage(root['遇见图床'],file_path)
+            if image_url != False:
+                os.remove(file_path)
+                return image_url
+
+        elif self.get_xclip(has_jpg) ==  0:
+            file_path = self.currentTime('.jpg')
+            self.xclip_path = file_path
+            self.get_xclip(save_jpg + file_path)
+            image_url = self.PostImage(root['遇见图床'],file_path)
+            if image_url != False:
+                os.remove(file_path)
+                return image_url
+
+        else:
+            print("error:no find xclip image",file=sys.stderr)
+            return False
+
+    def __del__(self):
+        if self.xclip_path != '':
+            os.remove(self.xclip_path)
+        pass
+
+
+def uploadPathImage(image):
+    tool = PostImage()
+    imageUrl = tool.PostImage(root['遇见图床'],image)
+    if imageUrl != False:
+        print("![](" + imageUrl +")")
+
+def uploadXclipImage():
+    tool = PostImage()
+    imageUrl = tool.getClipFileImage()
+    if imageUrl != False:
+        print("![](" + imageUrl +")")
+if __name__ == "__main__":
+    if len(sys.argv) ==  2:
+        uploadPathImage(sys.argv[1])
+    else:
+        uploadXclipImage()
